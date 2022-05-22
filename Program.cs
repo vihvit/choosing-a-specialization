4f091a8b798c54c59b50e89cef99b0b600a10635
Viktor Chernov <vihvit@mail.ru>
2022-05-22 15:22:24 +0700

    Creating a program

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿string[] array = new string []{"sfg","3577","fr45","we","(&","fdtdcjgc","dd"};
+PrintArray(array);
+void PrintArray(string[] currentArray)
+{   
+    for (int i=0; i < array.Length; i++)
+    {   if ( array[i].Length  <= 3) 
+        Console.Write(currentArray[i] +  " ");
+    }
+    Console.WriteLine();
+}
